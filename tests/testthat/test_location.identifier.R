8e60907e0ad8c5e783d24444c3c3f28c24a97610
Nikos Bosse <nikosbosse@gmail.com>
2018-08-03 17:19:59 +0200

    Test für die location identifier Funktion. Wahrscheinlich sollten wir uns erstmal auf die anderen, komplizierteren Funktionen konzentrieren.

--- tests/testthat/test_location.identifier.R
+++ tests/testthat/test_location.identifier.R
@@ -3,8 +3,9 @@ library(SAE)
 context("Testing of the function location identifier")
 
 test_that("The location identifier works with numbers", {
-  expect_equal(location)
-
-
-
+  expect_equal(location.simplifier(c("A", "b", "1")), c(1,2,3))
 })
+
+
+# unsinniger Test, weil der Code nicht kompliziert ist?
+
