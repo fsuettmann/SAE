6726503b40043ff66dcce8c3ec9c540b97ac665a
fsuettmann <fsuettmann@users.noreply.github.com>
2018-08-03 11:35:09 +0200

    this is just to try out pushing on git

---
+++ R/mean.for.regression.R
@@ -0,0 +1,7 @@
+# this function is supposed to automatically determine if a variable is metric or not
+# and then use the mean or median on the larger (census) sample to account for lacking
+# details in the survey sample.
+
+mean.for.regression <- function(mean.reg, censusdata){
+
+}
