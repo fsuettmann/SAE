f6aea519bc635f779b34046453b6cdfc909f5b00
Nikos Bosse <nikosbosse@gmail.com>
2018-08-07 20:11:54 +0200

    leichte Veränderung location.simplifier.function

--- R/location.simplifier.R
+++ R/location.simplifier.R
@@ -17,26 +17,31 @@
 # ACHTUNG: Das ergibt nur Sinn, wenn uns egal sind, wie Census- und
 # Survey Locations miteinander in Beziehung stehen
 
-location.simplifier <- function(surv_data, location) {
-  if(is.character(location) & length(location) == 1){
-    # if location is string this part is executed
-    t <- paste("surv_data$", location, sep = "")
-    loc <- as.factor(eval(parse(text=t)))
-    if(length(loc) != nrow(surv_data)){
-      stop('location has to be a string indicating one of the variabeles in the data set or a vector of the same length as the surveydata')
-      # hier wäre es vllt. wieder sinnvoll, die nrow()-Angabe
-      # einfach an die Funktion zu übergeben, dann muss das nicht neu berechnet werden
-      # auch sinnvoller wäre vllt. die Bedingung oben schon zu testen, ob der eingebene String überhaupt gültig ist?
-    }
-    levels(loc) <- seq.int(length.out = length(loc))
-    return(as.integer(loc))
-  } else if(length(location) == nrow(surv_data)) {
-    # if location is instead supplied as a separate vector
-    loc <- as.factor(location)
-    levels(loc) <- seq.int(length.out = length(loc))
-    return(as.integer(loc))
-  } else stop('location has to be a string indicating one of the variabeles in the data set or a vector of the same length as the surveydata')
-  # the function pasts an error if none of the two possible types of data are supplied
-  # or if there is no location given at all
-}
 
+location.simplifier <-
+  function(surv_data, location, n_obs_survey1) {
+    # if location is string this part is executed
+    if (is.character(location) & length(location) == 1) {
+      if (any(location == names(surv_data))) {
+        #only execute the following if the string specified is the name of a variable
+        t <- paste("surv_data$", location, sep = "")
+        loc <- as.factor(eval(parse(text = t)))
+      } else {
+        stop(
+          "String that was specified as location is not the name of one of the variables in the survey data set."
+        )
+      }
+      levels(loc) <- seq.int(length.out = length(loc))
+      return(as.integer(loc))
+    } else if (length(location) == n_obs_survey1) {
+      # if location is instead supplied as a separate vector
+      loc <- as.factor(location)
+      levels(loc) <- seq.int(length.out = length(loc))
+      return(as.integer(loc))
+    } else
+      stop(
+        'location has to be a string indicating one of the variabeles in the data set or a vector of the same length as the surveydata'
+      )
+    # the function pasts an error if none of the two possible types of data are supplied
+    # or if there is no location given at all
+  }
